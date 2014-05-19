7797975b85414f4350bf9fb10ae628f47bbc9b9b
Backbone-js-Demo <Backbone-js-Demo@users.noreply.github.com>
2014-05-19 09:36:32 +0200

    Update Bsp2.js

--- Bsp2.js
+++ Bsp2.js
@@ -2,30 +2,32 @@
 
 	//Definition View
 	//no other models defined
-  var ListView = Backbone.View.extend({
+	var ListView = Backbone.View.extend({
 	
-    el: $('body'), 
+		el: $('body'), //define that el is the body tag in the HTML page
+		
+		//Define Events and what should be done
+		events: {
+			'click button#add': 'addAddress' //if the button is pressed, the function 'addAddress' will be executed
+		},
+		
+		initialize: function(){
+			_.bindAll(this, 'render', 'addAddress'); //bind all functions
+		
+			this.counter = 0; //initialize counter
+			this.render(); //render view
+		},
+		
+		render: function(){
+			$(this.el).append("<button id='add'>Add new address</button>"); //create new button
+			$(this.el).append("<p></p>"); //add new paragraph
+		},
+		
+		addAddress: function(){
+			this.counter++; //increase counter by one
+			$('p', this.el).append("Address no."+this.counter+"<br>"); //append 'Line no.' and the current line number to the view
+		}
+	});
 	
-	//Define Events and what should be done
-    events: {
-      'click button#add': 'addAddress'
-    },
-	
-    initialize: function(){
-      _.bindAll(this, 'render', 'addAddress');
-
-      this.counter = 0; 
-      this.render();
-    },
-    render: function(){
-      $(this.el).append("<button id='add'>Add new address</button>");
-      $(this.el).append("<p></p>");
-    },
-    addAddress: function(){
-      this.counter++;
-      $('p', this.el).append("Address no."+this.counter+"<br>");
-    }
-  });
-
-  var listView = new ListView();
+	var listView = new ListView(); //call ListView
 })(jQuery);
