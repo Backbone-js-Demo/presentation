bb9b1593f46822a4507b015f817bd758a67e0f6e
Backbone-js-Demo <Backbone-js-Demo@users.noreply.github.com>
2014-05-19 09:31:18 +0200

    Update Bsp1.js

--- Bsp1.js
+++ Bsp1.js
@@ -6,16 +6,16 @@
   var ListView = Backbone.View.extend({
     el: $('body'), 
 
-	initialize: function(){
-      _.bindAll(this, 'render'); 
+	initialize: function(){  //initialize View
+      _.bindAll(this, 'render'); //bind everything rendered to the body
 
-       this.render(); 
+       this.render(); //render View
     },
 
 	render: function(){
-      $(this.el).append("<h1>Hello Backbone.js!</h1>");
+      $(this.el).append("<h1>Hello Backbone.js!</h1>"); //append 'Hello Backbone.js to the body of the HTML page
     }
   });
 
-  var listView = new ListView();
+  var listView = new ListView(); //run View
 })(jQuery);
